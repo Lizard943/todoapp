99da5e5fab9305353f7d54522ec40048aca3cfbc
Lizard943 <vuliz943@gmail.com>
2025-08-09 22:57:22 +0700

    add fetch data

---
+++ src/views/home/DailyTaskView.vue
@@ -0,0 +1,37 @@
+<template lang="">
+  <div class="px-5 pt-5 font-monaSan">
+    <SearchBar />
+    <div class="mt-5 flex flex-col gap-2">
+      <h2 class="text-2xl font-semibold">Daily Tasks</h2>
+      <div class="flex flex-row justify-between items-center">
+        <p class="text-gray-500">Manage your daily tasks efficiently</p>
+        <DailyTaskCreateItem @updated="fetchData" />
+      </div>
+    </div>
+    <DailyTaskTable :tasks="tasks" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUpdate } from 'vue'
+import axios from 'axios'
+import SearchBar from '@/components/mytask/SearchBar.vue'
+import DailyTaskTable from '@/components/dailytask/DailyTaskTable.vue'
+import DailyTaskCreateItem from '@/components/dailytask/DailyTaskCreateItem.vue'
+const tasks = ref(null)
+const error = ref('')
+onMounted(() => {
+  fetchData()
+})
+onBeforeUpdate(() => {
+  fetchData()
+})
+const fetchData = async () => {
+  try {
+    const res = await axios.get('http://localhost:3000/api/daily-task/group-1')
+    tasks.value = res.data
+  } catch (err) {
+    error.value = err instanceof Error ? err.message : String(err)
+  }
+}
+</script>
